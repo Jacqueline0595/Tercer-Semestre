e5b3b9e2a39ad33e2a3dc0298de541f14035cb2c
Jack0595 <janellejackelinegomezbenitez@gmail.com>
2025-01-30 08:16:42 -0600

    Tareas

---
+++ Estructura de datos/Tareas/tarea3.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Autora: Janelle Jacqueline Gomez Benitez
+// Descripcion: Escriba un programa que contenga las funciones básicas para manejar 
+// un apuntador a un apuntador a un apuntador a un dato de tipo unsigned.
+// Notas: Usar %u  como especificador de tipo.
+
+int asignaMem(unsigned ****ptr);
+void asignaDato(unsigned ***ptr, unsigned dato);
+void pideDato(unsigned ***ptr);
+void muestraDato(unsigned ***ptr);
+void aumentaUno(unsigned ***ptr);
+void liberaMem(unsigned ***ptr);
+
+int main()
+{  
+    unsigned ***apuntador;
+    int res;
+    res = asignaMem(&apuntador);
+    if(res)
+    {
+        asignaDato(apuntador, 25);
+        muestraDato(apuntador);
+        pideDato(apuntador);
+        muestraDato(apuntador);
+        aumentaUno(apuntador);
+        muestraDato(apuntador);
+        liberaMem(apuntador);
+    }
+
+    return 0;
+}
+
+int asignaMem(unsigned ****ptr)
+ 
+{
+    int res=0;
+    *ptr = (unsigned ***)malloc(sizeof(unsigned **));
+    if(*ptr)
+    {
+        **ptr = (unsigned **)malloc(sizeof(unsigned *));
+        if(**ptr)
+        {
+            ***ptr = (unsigned *)malloc(sizeof(unsigned));
+            if(***ptr)
+                res = 1;
+            else
+                free(**ptr);
+        } else
+            free(*ptr);
+    }
+    return res;
+}
+
+void asignaDato(unsigned ***ptr, unsigned dato)
+{
+    ***ptr = dato;
+}
+
+void pideDato(unsigned ***ptr)
+{
+    printf("Dame un dato: \n");
+    scanf("%u", **ptr);
+}
+
+void muestraDato(unsigned ***ptr)
+{
+    printf("El dato del puntero es: %u \n", ***ptr);
+}
+
+void aumentaUno(unsigned ***ptr)
+{
+    (***ptr)++;
+}
+
+void liberaMem(unsigned ***ptr)
+{
+    free(ptr);
+}
---
+++ Estructura de datos/Tareas/tarea4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define NChar 25
+
+typedef struct VENDE
+{
+    int clave;
+    char nombre[NChar];
+    float totVen;
+    float suelBas;
+    int comision;
+    float suelNet;
+};
+
+
+int main()
+{
+    
+    return 0;
+}
