c61e3946cdd1f3bec932f5b0d5cd14d503fe72f2
Jack0595 <janellejackelinegomezbenitez@gmail.com>
2025-02-27 22:13:25 -0600

    Cambios de Gauss Jordan

--- Analisis/proyecto.py
+++ Analisis/proyecto.py
@@ -1,13 +1,14 @@
 import numpy as np  
+from fractions import Fraction
 
 def leerMatriz(nombre_archivo):
     with open(nombre_archivo, 'r') as f:
         lineas = f.readlines()
     
     n, m = map(int, lineas[0].strip().split()) 
-    matriz = [list(map(float, linea.split())) for linea in lineas[1:n+1]]
+    matriz = [list(map(lambda x: Fraction(x), linea.split())) for linea in lineas[1:n+1]]
     
-    return np.array(matriz)  
+    return np.array(matriz, dtype=object)  
 
 def gaussJordan(matriz, archivo):
     n, m = matriz.shape 
@@ -20,13 +21,14 @@ def gaussJordan(matriz, archivo):
         for i in range(n): 
             piv = matriz[i, i]  
             f.write(f"Dividimos la fila {i + 1} por {piv} para hacer el pivote\n")
-            matriz[i] /= piv  
+            matriz[i] = [x / piv for x in matriz[i]]  
             
             for j in range(n):  
                 if i != j and j < n:
                     factor = matriz[j, i]  
-                    f.write(f"A la fila {j + 1} le restamos {factor}  y multiplicamos por la fila {i + 1}\n")
-                    matriz[j] -= factor * matriz[i]  
+                    f.write(f"A la fila {j + 1} le restamos {factor} y multiplicamos por la fila {i + 1}\n")
+                    matriz[j] = [matriz[j, k] - factor * matriz[i, k] for k in range(m)]  
+            
             f.write("\nDespues de la operacion:\n")
             for fila in matriz:
                 f.write(" ".join(map(str, fila)) + "\n")
@@ -39,7 +41,7 @@ def gaussJordan(matriz, archivo):
     return matriz  
 
 def main():
-    archivo = r"C:\Users\jacqu\3\Tercer-Semestre\Analisis\matriz.txt"
+    archivo = r"C:\\Users\\jacqu\\3\\Tercer-Semestre\\Analisis\\matriz.txt"
     
     datos = leerMatriz(archivo)
     gaussJordan(datos, archivo)
